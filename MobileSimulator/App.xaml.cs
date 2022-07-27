1ac44b02c88fbf50c29b057f6fd9f41c2265c7b7
DavidPollak <49835466+DavidPollak@users.noreply.github.com>
2022-07-27 20:56:38 +0300

    Add files via upload

---
+++ MobileSimulator/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using MobileSimulator.Infrastructure.Interfaces;
+using MobileSimulator.Infrastructure.Ioc;
+using MobileSimulator.ViewModels.ViewModels;
+
+namespace MobileSimulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public Window MainStartWindow;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            log4net.Config.XmlConfigurator.Configure();
+
+            LoadAdditionalResourceDictionaries();
+            MainStartWindow = new MainWindow();
+            var context = new MainWindowViewModel();
+            MainStartWindow.DataContext = context;
+            MainStartWindow.Show();
+        }
+
+        private void LoadAdditionalResourceDictionaries()
+        {
+            var loaders = MefLoader.GetExportedValues<IAdditionalResourceLoader>();
+            foreach (var loader in loaders)
+            {
+                loader.Load(Resources.MergedDictionaries);
+            }
+        }
+    }
+}
---
+++ MobileSimulator/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using MobileSimulator.ViewModels.ViewModels;
+
+namespace MobileSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnActivated(object sender, EventArgs e)
+        {
+            // Show overlay if we lose focus
+            var mainWindowViewModel = DataContext as MainWindowViewModel;
+            if (mainWindowViewModel != null && mainWindowViewModel.DimmableOverlayVisible)
+            {
+
+                mainWindowViewModel.DimmableOverlayVisible = false;
+            }
+        }
+
+        private void MainWindow_OnDeactivated(object sender, EventArgs e)
+        {
+            if (Application.Current.Windows.Count > 1)
+            {
+                // Show overlay if we lose focus
+                var mainWindowViewModel = DataContext as MainWindowViewModel;
+                if (mainWindowViewModel != null)
+                    mainWindowViewModel.DimmableOverlayVisible = true;
+            }
+        }
+    }
+}
